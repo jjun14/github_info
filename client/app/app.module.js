5e552750166074f7636f388dea54c0b1027372b1
Jimmy Jun <jimmy.s.jun@gmail.com>
2016-03-14 15:35:04 -0700

    Set up repos controller

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -122,4 +122,24 @@
       });
     }
   }
+
+  // repos controller
+  angular
+    .module('github-notetaker')
+    .controller('reposController', reposController);
+
+  reposController.$inject = ['githubFactory'];
+
+  function reposController(githubFactory){
+    var repos = this;
+
+    init();
+
+    function init(){
+      githubFactory.getUserRepos(function(data){
+        repos.userRepos = data;
+        console.log(repos.userRepos);
+      })
+    }
+  }
 })();
