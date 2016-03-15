3eeccadda5e6ce2c37e63d026050937e9df3bb26
Jimmy Jun <jimmy.s.jun@gmail.com>
2016-03-15 14:33:15 -0700

    Finished retrieving notes from db

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -30,7 +30,8 @@
                 controller: "reposController as repos"
               },
               "notes": {
-                templateUrl: "partials/user.notes.html"
+                templateUrl: "partials/user.notes.html",
+                controller: 'notesController as notes'
               }
             }
           })
@@ -140,6 +141,50 @@
       githubFactory.getUserRepos(function(data){
         repos.userRepos = data;
         console.log(repos.userRepos);
+      })
+    }
+  }
+
+  // notes factory
+  angular
+    .module('github-notetaker')
+    .factory('notesFactory', notesFactory);
+
+  notesFactory.$inject = ['$http'];
+
+  function notesFactory($http){
+    var notes = [];
+    var factory = {
+      getNotesForUser: getNotesForUser
+    }
+
+    return factory;
+
+    function getNotesForUser(username, callback){
+      $http.get('/notes/'+username).success(function(data){
+        notes = data;
+        callback(notes);
+      });
+    }
+  }
+
+  // notes controller
+  angular
+    .module('github-notetaker')
+    .controller('notesController', notesController);
+
+  notesController.$inject = ['notesFactory', '$state']
+
+  function notesController(notesFactory, $state){
+    console.log($state.params);
+    var notes = this;
+
+    init();
+
+    function init(){
+      notesFactory.getNotesForUser($state.params.username, function(data){
+        notes.userNotes = data;
+        console.log(notes.userNotes);
       })
     }
   }
--- server/server.js
+++ server/server.js
@@ -5,7 +5,26 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var pgp = require('pg-promise')({});
+var cn = {
+  host: 'localhost',
+  port: 5432,
+  database: "github_notetaker",
+  user: "jimmysjun",
+  password: "codingdojo15",
+}
+var db = pgp(cn);
+
 app.use(express.static(path.join(__dirname, "../client")));
+
+app.get('/notes/:username', function(req, res){
+  console.log(req.params);
+  db.any("SELECT * FROM notes WHERE username=$1", req.params.username)
+    .then(function(data){
+      console.log("DATA", data);
+      res.json(data);
+    })
+})
 
 app.listen(8000, function(){
   console.log("listening on port 8000");
